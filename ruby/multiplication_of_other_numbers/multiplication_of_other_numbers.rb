45fe050c5df771a8b282330a3a55ebe7588b407e
Peter Toth <tothpeter08@gmail.com>
2015-10-14 17:53:17 +0200

    Add 2 loops version of multiplication of other numbers

--- ruby/multiplication_of_other_numbers/multiplication_of_other_numbers.rb
+++ ruby/multiplication_of_other_numbers/multiplication_of_other_numbers.rb
@@ -1,4 +1,4 @@
-def exercise input
+def exercise_with_one_loop input
   result = [1] * input.length
 
   i = 1
@@ -16,6 +16,28 @@ def exercise input
 
     i += 1
     j -= 1
+  end
+
+  result
+end
+
+def exercise_with_two_loops input
+  result = [1] * input.length
+
+  i = 1
+
+  while i < input.length
+    result[i] = result[i - 1] * input[i - 1]
+    i += 1
+  end
+
+  multiplication_left = 1
+  i = input.length - 2
+
+  while i > -1
+    multiplication_left = multiplication_left * input[i + 1]
+    result[i] = multiplication_left * result[i]
+    i -= 1
   end
 
   result
--- ruby/multiplication_of_other_numbers/spec/multiplication_of_other_numbers_spec.rb
+++ ruby/multiplication_of_other_numbers/spec/multiplication_of_other_numbers_spec.rb
@@ -1,11 +1,21 @@
 require './multiplication_of_other_numbers'
 
-describe 'exercise' do
+describe 'exercise_with_one_loop' do
   it "returns the correct array" do
     input = [2, 3, 2, 3]
-    expect(exercise(input)).to eq [18, 12, 18, 12]
+    expect(exercise_with_one_loop(input)).to eq [18, 12, 18, 12]
 
     input = [7, 1, 2, 6, 3]
-    expect(exercise(input)).to eq [36, 252, 126, 42, 84]
+    expect(exercise_with_one_loop(input)).to eq [36, 252, 126, 42, 84]
+  end
+end
+
+describe 'exercise_with_two_loops' do
+  it "returns the correct array" do
+    input = [2, 3, 2, 3]
+    expect(exercise_with_two_loops(input)).to eq [18, 12, 18, 12]
+
+    input = [7, 1, 2, 6, 3]
+    expect(exercise_with_two_loops(input)).to eq [36, 252, 126, 42, 84]
   end
 end
