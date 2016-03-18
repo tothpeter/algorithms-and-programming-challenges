cbf0554df3a9ba80ecab72272a4595c80e948db0
Peter Toth <tothpeter08@gmail.com>
2016-03-18 20:50:12 +0100

    Implement basic add new todo functionality

--- javascript/todo/js/app.js
+++ javascript/todo/js/app.js
@@ -1,16 +1,25 @@
 var App = Base.extend({
   init: function(state) {
     this.state = state || {input: {value: ''}};
+    var app = this;
 
     this.input = new Input(this.state.input);
     this.input.onReturnPressed(function(event) {
-      console.log('New todo text:', this.state.value);
+      app._addItem(this.state.value);
     });
+
+    this.list = new TodoList();
+  },
+
+  _addItem: function(todoTitle) {
+    var newTodoItem = new TodoItem({title: todoTitle});
+    this.list.addItem(newTodoItem);
   },
 
   render: function() {
     var $container = document.getElementById('container');
 
     $container.appendChild(this.input.render());
+    $container.appendChild(this.list.render());
   }
 });
