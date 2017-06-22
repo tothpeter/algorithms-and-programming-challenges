cd1902f753788df9f7f8595c7efd2c9cc6082b84
Peter Toth <tothpeter08@gmail.com>
2017-06-22 15:29:59 +0200

    Add stock-price java practice

---
+++ practice_java/test_src/interview_cake/P01StockPriceTest.java
@@ -0,0 +1,34 @@
+package interview_cake;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import interview_cake.P01StockPrice;
+
+public class P01StockPriceTest {
+
+	@Test
+	public void testNormalCase() {
+		int[] stockPricesYesterday = new int[] { 10, 7, 5, 8, 11, 9 };
+		
+		int result = P01StockPrice.getMaxProfit(stockPricesYesterday);
+		
+		assertEquals(6, result);
+	}
+	
+	@Test
+	public void testWhenStockValueGoesDownAllDay() {
+		int[] stockPricesYesterday = new int[] { 10, 7 };
+		
+		int result = P01StockPrice.getMaxProfit(stockPricesYesterday);
+		
+		assertEquals(-3, result);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)  
+	public void testThrowsAnException() {
+		P01StockPrice.getMaxProfit(new int[] {1});
+	}
+
+}
