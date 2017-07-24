52ed6fd85f4d486e86e7156b412f817bd9367735
Peter Toth <tothpeter08@gmail.com>
2017-07-24 20:42:00 +0200

    Covert one test to use Spectrum test runner and RSpec like categorisation

--- practice_java/src/test/java/interview_cake/P32TopScoresTest.java
+++ practice_java/src/test/java/interview_cake/P32TopScoresTest.java
@@ -4,28 +4,34 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class P32TopScoresTest {
+import com.greghaskins.spectrum.Spectrum;
+import static com.greghaskins.spectrum.dsl.specification.Specification.*;
 
-	@Test
-	public void testGetSortedScores() {
-		int[] unsortedScores = { 37, 89, 41, 65, 91, 53 };
-		int[] expectedResult = new int[] { 91, 89, 65, 53, 41, 37 };
-		
-		int[] result = P32TopScores.getSortedScores(unsortedScores, 100);
-		
-		assertEquals(Arrays.toString(expectedResult), Arrays.toString(result));
-	}
-	
-	@Test
-	public void testGetSortedScoresWhenWeHaveDuplicatedScores() {
-		int[] unsortedScores = { 3, 3, 5, 4 };
-		int[] expectedResult = new int[] { 5, 4, 3, 3 };
-		
-		int[] result = P32TopScores.getSortedScores(unsortedScores, 5);
-		
-		assertEquals(Arrays.toString(expectedResult), Arrays.toString(result));
-	}
+@RunWith(Spectrum.class)
+public class P32TopScoresTest {{
+	describe(".getSortedScores", () -> {
+		describe("When we do not have duplicated scores", () -> {
+			it("returns the sorted list", () -> {
+				int[] unsortedScores = { 37, 89, 41, 65, 91, 53 };
+				int[] expectedResult = new int[] { 91, 89, 65, 53, 41, 37 };
+				
+				int[] result = P32TopScores.getSortedScores(unsortedScores, 100);
+				
+				assertEquals(Arrays.toString(expectedResult), Arrays.toString(result));
+			});
+		});
 
-}
+		describe("When we have duplicated scores", () -> {
+			it("returns the sorted list", () -> {
+				int[] unsortedScores = { 3, 3, 5, 4 };
+				int[] expectedResult = new int[] { 5, 4, 3, 3 };
+				
+				int[] result = P32TopScores.getSortedScores(unsortedScores, 5);
+				
+				assertEquals(Arrays.toString(expectedResult), Arrays.toString(result));
+			});
+		});
+	});
+}}
