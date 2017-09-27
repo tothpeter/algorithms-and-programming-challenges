d2955bf1d9d8a69e2e4e933cad6dbf11ac16cb44
Peter Toth <tothpeter08@gmail.com>
2017-09-27 22:54:54 +0200

    Add finding missing element in list ruby challenge

---
+++ ruby/codility.com/lesson_3/challenge1.rb
@@ -0,0 +1,9 @@
+def solution list
+  return 1 if list.empty?
+
+  current_sum_of_list = list.inject :+
+
+  expected_sum_of_list = (list.first + list.last) * (list.length + 1) / 2
+
+  expected_sum_of_list - current_sum_of_list
+end
---
+++ ruby/codility.com/lesson_3/spec/challenge1_spec.rb
@@ -0,0 +1,25 @@
+require './challenge1'
+
+describe 'solution' do
+  context "when the list is empty" do
+    it 'returns 1' do
+      input           = []
+      expected_output = 1
+
+      output = solution input
+
+      expect(output).to eq(expected_output)
+    end
+  end
+
+  context "when the list is not empty" do
+    it 'returns the missing item' do
+      input           = [ 1, 2, 3, 5 ]
+      expected_output = 4
+
+      output = solution input
+
+      expect(output).to eq(expected_output)
+    end
+  end
+end
