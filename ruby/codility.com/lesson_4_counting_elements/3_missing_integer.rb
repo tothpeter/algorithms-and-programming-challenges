4d50619362d0bc4dceb22d9e642a5cb288d133db
Peter Toth <tothpeter08@gmail.com>
2017-10-06 22:22:09 +0200

    Remove bad solution

--- ruby/codility.com/lesson_4_counting_elements/3_missing_integer.rb
+++ ruby/codility.com/lesson_4_counting_elements/3_missing_integer.rb
@@ -1,9 +1,7 @@
-# Counting Elements / MissingInteger
 # Find the smallest positive integer that does not occur in a given sequence
 
 require "set"
 
-# Optimized for computing
 def solution1 list
   seen = Set.new list
   max = list.max
@@ -15,22 +13,4 @@ def solution1 list
   end
 
   return max + 1
-end
-
-# Optimized for storage
-def solution2 list
-  min = 1
-
-  list.select! &:positive?
-  list = list.uniq.sort
-
-  list.length.times do |index|
-    if list[index] != min
-      return index + 1
-    end
-
-    min += 1
-  end
-
-  min
 end
