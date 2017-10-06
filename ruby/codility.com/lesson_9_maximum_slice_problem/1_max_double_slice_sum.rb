70cb3b4d071d55ef5603008a0345d50941144d60
Peter Toth <tothpeter08@gmail.com>
2017-10-06 17:32:15 +0200

    Start working on new challenge

---
+++ ruby/codility.com/lesson_9_maximum_slice_problem/1_max_double_slice_sum.rb
@@ -0,0 +1,8 @@
+# Find the maximal sum of any double slice.
+# 12
+
+def solution1 list
+  return 0 if list.length < 4
+
+  
+end
---
+++ ruby/codility.com/lesson_9_maximum_slice_problem/spec/1_max_double_slice_sum_spec.rb
@@ -0,0 +1,19 @@
+require './1_max_double_slice_sum'
+require '../../spec_helper'
+
+examples = [
+  {
+    context: 'comprehensive case',
+    input: [ [ 3, 2, 6, -1, 4, 5, -1, 2 ] ],
+    expected_output: 17
+  },
+  {
+    context: 'when we have less items than 4',
+    input: [ [ 1, 2, 3 ] ],
+    expected_output: 0
+  }
+]
+
+describe "solution1" do
+  test_batch_examples examples, :solution1
+end
