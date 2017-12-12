efbd918fd3aaff889c028006addd8e6c45c2e68e
Peter Toth <tothpeter08@gmail.com>
2017-12-12 16:41:38 +0100

    Refactor Fibonacci Ruby exercise

--- ruby/fibonacci/fibonacci.rb
+++ ruby/fibonacci/fibonacci.rb
@@ -1,62 +1,38 @@
+# 0, 1, 2, 3, 4, 5, 6, 7,  8
+# 0, 1, 1, 2, 3, 5, 8, 13, 21
+
 class Fib
-  # 0,1,1,2,3,5,8,13,21
-  # 0,1,2,3,4,5,6,7, 8
-  
-  def self.recursive nth_number
-    if nth_number == 0
-      return 0
-    elsif nth_number == 1
-      return 1
-    else
-      return recursive(nth_number - 1) + recursive(nth_number - 2)
-    end
+  def self.recursive n
+    return n if n < 2
+
+    recursive(n - 1) + recursive(n - 2)
   end
 
-  def self.iterative nth_number
-    if nth_number == 0
-      return 0
-    elsif nth_number == 1 || nth_number == 2
-      return 1
-    else
-      item_0 = 0
-      item_1 = 1
-      current = 0
+  def self.iterative n
+    return n if n < 2
 
-      (2..nth_number).each do
-        current = item_0 + item_1
-        item_0 = item_1
-        item_1 = current
-      end
-      
-      current
+    item_0 = 0
+    item_1 = 1
+    current = nil
+
+    (n - 1).times do
+      current = item_0 + item_1
+      item_0 = item_1
+      item_1 = current
     end
+
+    current
   end
 end
 
 class Fiber
-
   def initialize
-    @memo = {}
+    @memo = { 0 => 0, 1 => 1 }
   end
-  
-  def recursive nth_number
 
-    if nth_number == 0 || nth_number == 1
-      return nth_number
-    end
-    
-    if @memo.has_key? nth_number
-      p "Grabbing memo for: #{nth_number}"
-      return @memo[nth_number]
-    end
+  def recursive_with_memoize n
+    return @memo[n] if @memo.has_key? n
 
-    p "computing fib #{nth_number}"
-
-    result = recursive(nth_number - 1) + recursive(nth_number - 2)
-
-    # Memoize
-    @memo[nth_number] = result
-
-    return result
+    @memo[n] = recursive_with_memoize(n - 1) + recursive_with_memoize(n - 2)
   end
 end
--- ruby/fibonacci/spec/fibonacci_spec.rb
+++ ruby/fibonacci/spec/fibonacci_spec.rb
@@ -2,37 +2,41 @@ require './fibonacci'
 
 describe Fib do
   describe ".recursive" do
-    it "returns the nth fibonacci" do
-      expect(Fib.recursive 0).to eq 0
-      expect(Fib.recursive 1).to eq 1
-      expect(Fib.recursive 2).to eq 1
-      expect(Fib.recursive 5).to eq 5
-      expect(Fib.recursive 7).to eq 13
-      expect(Fib.recursive 8).to eq 21
+    it "returns the nth fibonacci number" do
+      expect(Fib.recursive(0)).to eq(0)
+      expect(Fib.recursive(1)).to eq(1)
+      expect(Fib.recursive(2)).to eq(1)
+      expect(Fib.recursive(5)).to eq(5)
+      expect(Fib.recursive(7)).to eq(13)
+      expect(Fib.recursive(8)).to eq(21)
     end
   end
 
   describe ".iterative" do
-    it "returns the nth fibonacci" do
-      expect(Fib.iterative 0).to eq 0
-      expect(Fib.iterative 1).to eq 1
-      expect(Fib.iterative 2).to eq 1
-      expect(Fib.iterative 5).to eq 5
-      expect(Fib.iterative 7).to eq 13
-      expect(Fib.iterative 8).to eq 21
+    it "returns the nth fibonacci number" do
+      expect(Fib.iterative(0)).to eq(0)
+      expect(Fib.iterative(1)).to eq(1)
+      expect(Fib.iterative(2)).to eq(1)
+      expect(Fib.iterative(5)).to eq(5)
+      expect(Fib.iterative(7)).to eq(13)
+      expect(Fib.iterative(8)).to eq(21)
     end
   end
 end
 
 describe Fiber do
-  describe ".recursive" do
-    it "returns the nth fibonacci" do
-      expect(Fiber.new.recursive 0).to eq 0
-      expect(Fiber.new.recursive 1).to eq 1
-      expect(Fiber.new.recursive 2).to eq 1
-      expect(Fiber.new.recursive 5).to eq 5
-      expect(Fiber.new.recursive 7).to eq 13
-      expect(Fiber.new.recursive 8).to eq 21
+  describe "#recursive_with_memoize" do
+    it "returns the nth fibonacci number" do
+      expect(Fiber.new.recursive_with_memoize(0)).to eq(0)
+      expect(Fiber.new.recursive_with_memoize(1)).to eq(1)
+      expect(Fiber.new.recursive_with_memoize(2)).to eq(1)
+      expect(Fiber.new.recursive_with_memoize(5)).to eq(5)
+      expect(Fiber.new.recursive_with_memoize(7)).to eq(13)
+      expect(Fiber.new.recursive_with_memoize(8)).to eq(21)
+    end
+
+    it "runs fast" do
+      expect(Fiber.new.recursive_with_memoize(60)).to eq(1548008755920)
     end
   end
 end
