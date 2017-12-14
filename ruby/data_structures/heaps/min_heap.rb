99e24288968b20af45f40c70ea5ba2147dc50dbb
Peter Toth <tothpeter08@gmail.com>
2017-12-14 16:34:22 +0100

    Enhance min heap ruby implementation

--- ruby/data_structures/heaps/min_heap.rb
+++ ruby/data_structures/heaps/min_heap.rb
@@ -2,12 +2,16 @@ class MinHeap
   attr_accessor :items
 
   def initialize
-    @items    = []
-    @size     = 0
+    @items = []
+    @size  = 0
   end
 
   def add_item item
+    @items[@size] = item
+
     @size += 1
+
+    heapify_up
   end
 
   def add_items items
@@ -21,54 +25,88 @@ class MinHeap
   end
 
   def poll
-    return nil if size == 0
+    return nil if @size == 0
 
     result = @items.first
 
+    @items[0] = @items[@size - 1]
 
+    @size -= 1
+
+    heapify_down
 
     result
   end
 
   private
 
-  def get_left_child_index parent_index
+  def left_child_index parent_index
     2 * parent_index + 1
   end
 
-  def get_right_child_index parent_index
+  def right_child_index parent_index
     2 * parent_index + 2
   end
 
-  def get_parent_index child_index
+  def parent_index child_index
     (child_index - 1) / 2
   end
 
-  def has_left_child parent_index
-    get_left_child_index(parent_index) < @size
+  def has_left_child? parent_index
+    left_child_index(parent_index) < @size
   end
 
-  def has_right_child parent_index
-    get_right_child_index(parent_index) < @size
+  def has_right_child? parent_index
+    right_child_index(parent_index) < @size
   end
 
-  def has_parent child_index
-    get_parent_index(child_index) >= 0
+  def has_parent? child_index
+    child_index > 0
   end
 
-  def get_left_child parent_index
-    @items[get_left_child_index(parent_index)]
+  def left_child parent_index
+    @items[left_child_index(parent_index)]
   end
 
-  def get_right_child parent_index
-    @items[get_right_child_index(parent_index)]
+  def right_child parent_index
+    @items[right_child_index(parent_index)]
   end
 
-  def get_parent child_index
-    @items[get_parent_index(child_index)]
+  def parent child_index
+    @items[parent_index(child_index)]
   end
 
   def swap index_a, index_b
     @items[index_a], @items[index_b] = @items[index_b], @items[index_a]
+  end
+
+  def heapify_up
+    current_index = @size - 1
+
+    while has_parent?(current_index) && parent(current_index) > @items[current_index]
+      parent_index = parent_index(current_index)
+
+      swap(parent_index, current_index)
+      current_index = parent_index
+    end
+  end
+
+  def heapify_down
+    current_index = 0
+
+    while has_left_child?(current_index)
+      smaller_child_index = left_child_index(current_index)
+
+      if has_right_child?(current_index) && right_child(current_index) < left_child(current_index)
+        smaller_child_index = right_child_index(current_index)
+      end
+
+      if @items[smaller_child_index] < @items[current_index]
+        swap(smaller_child_index, current_index)
+        current_index = smaller_child_index
+      else
+        break
+      end
+    end
   end
 end
--- ruby/data_structures/heaps/spec/min_heap_spec.rb
+++ ruby/data_structures/heaps/spec/min_heap_spec.rb
@@ -1,91 +1,115 @@
 require './min_heap'
 
 describe MinHeap do
-  # describe '#add' do
-  #   it "works" do
-  #     heap = MinHeap.new 100
-  #
-  #     expect(heap.min).to eq(nil)
-  #
-  #     heap.add 9
-  #     expect(heap.min).to eq(9)
-  #
-  #     heap.add 3
-  #     expect(heap.min).to eq(3)
-  #
-  #
-  #     heap.add 7
-  #     expect(heap.min).to eq(3)
-  #
-  #     heap.add 9
-  #     expect(heap.min).to eq(3)
-  #
-  #     heap.add 4
-  #     expect(heap.min).to eq(3)
-  #
-  #     heap.add 2
-  #     expect(heap.min).to eq(2)
-  #   end
-  # end
+  describe 'integration' do
+    describe '#add' do
+      it "adds the new elements to the right place" do
+        heap = MinHeap.new
+
+        expect(heap.peak).to eq(nil)
+
+        heap.add_item 9
+        expect(heap.peak).to eq(9)
+
+        heap.add_item 3
+        expect(heap.peak).to eq(3)
+
+
+        heap.add_item 7
+        expect(heap.peak).to eq(3)
+
+        heap.add_item 9
+        expect(heap.peak).to eq(3)
+
+        heap.add_item 4
+        expect(heap.peak).to eq(3)
+
+        heap.add_item 2
+        expect(heap.peak).to eq(2)
+      end
+    end
+
+    describe '#poll' do
+      it 'removes the smallest element and returns it' do
+        heap = MinHeap.new
+
+        heap.add_items [ 0, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10 ]
+        heap.items   = [ 0, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10 ]
+
+        expect(heap.poll).to eq(0)
+        expect(heap.poll).to eq(1)
+        expect(heap.poll).to eq(2)
+        expect(heap.poll).to eq(3)
+        expect(heap.poll).to eq(4)
+        expect(heap.poll).to eq(5)
+        expect(heap.poll).to eq(6)
+        expect(heap.poll).to eq(7)
+        expect(heap.poll).to eq(8)
+        expect(heap.poll).to eq(9)
+        expect(heap.poll).to eq(10)
+        expect(heap.poll).to eq(nil)
+      end
+    end
+  end
 
   describe 'helper methods' do
     before do
       @heap = MinHeap.new
 
-      @heap.add_items [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-      @heap.items = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+      @heap.add_items [ 0, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10 ]
+      @heap.items   = [ 0, 2, 1, 4, 3, 5, 6, 7, 8, 9, 10 ]
     end
 
     let(:heap) { @heap }
 
-    describe '#get_left_child_index' do
-      it "works" do
-        expect(heap.send(:get_left_child_index, 0)).to eq(1)
-        expect(heap.send(:get_left_child_index, 1)).to eq(3)
-        expect(heap.send(:get_left_child_index, 2)).to eq(5)
-        expect(heap.send(:get_left_child_index, 4)).to eq(9)
+    describe '#left_child_index' do
+      it "returns the proper index" do
+        expect(heap.send(:left_child_index, 0)).to eq(1)
+        expect(heap.send(:left_child_index, 1)).to eq(3)
+        expect(heap.send(:left_child_index, 2)).to eq(5)
+        expect(heap.send(:left_child_index, 4)).to eq(9)
       end
     end
 
-    describe '#get_right_child_index' do
-      it "works" do
-        expect(heap.send(:get_right_child_index, 0)).to eq(2)
-        expect(heap.send(:get_right_child_index, 1)).to eq(4)
-        expect(heap.send(:get_right_child_index, 2)).to eq(6)
-        expect(heap.send(:get_right_child_index, 4)).to eq(10)
+    describe '#right_child_index' do
+      it "returns the proper index" do
+        expect(heap.send(:right_child_index, 0)).to eq(2)
+        expect(heap.send(:right_child_index, 1)).to eq(4)
+        expect(heap.send(:right_child_index, 2)).to eq(6)
+        expect(heap.send(:right_child_index, 4)).to eq(10)
       end
     end
 
-    describe '#get_parent_index' do
-      it "works" do
-        expect(heap.send(:get_parent_index, 1)).to eq(0)
-        expect(heap.send(:get_parent_index, 2)).to eq(0)
-        expect(heap.send(:get_parent_index, 8)).to eq(3)
-        expect(heap.send(:get_parent_index, 9)).to eq(4)
+    describe '#parent_index' do
+      it "returns the proper index" do
+        expect(heap.send(:parent_index, 1)).to eq(0)
+        expect(heap.send(:parent_index, 2)).to eq(0)
+        expect(heap.send(:parent_index, 8)).to eq(3)
+        expect(heap.send(:parent_index, 9)).to eq(4)
       end
     end
 
-    describe '#has_left_child' do
-      it "works" do
-        expect(heap.send(:has_left_child, 0)).to eq(true)
-        expect(heap.send(:has_left_child, 6)).to eq(false)
-        expect(heap.send(:has_left_child, 7)).to eq(false)
+    describe '#has_left_child?' do
+      it "returns the proper index" do
+        expect(heap.send(:has_left_child?, 0)).to eq(true)
+        expect(heap.send(:has_left_child?, 6)).to eq(false)
+        expect(heap.send(:has_left_child?, 7)).to eq(false)
       end
     end
 
-    describe '#has_right_child' do
-      it "works" do
-        expect(heap.send(:has_right_child, 0)).to eq(true)
-        expect(heap.send(:has_right_child, 6)).to eq(false)
-        expect(heap.send(:has_right_child, 7)).to eq(false)
+    describe '#has_right_child?' do
+      it "returns the proper index" do
+        expect(heap.send(:has_right_child?, 0)).to eq(true)
+        expect(heap.send(:has_right_child?, 6)).to eq(false)
+        expect(heap.send(:has_right_child?, 7)).to eq(false)
       end
     end
 
     describe '#swap' do
-      it "works" do
+      it "returns the proper index" do
         heap.send(:swap, 0, 2)
 
-        expect(heap.items[0]).to eq(2)
+        expect(heap.items[0]).to eq(1)
         expect(heap.items[2]).to eq(0)
       end
     end
