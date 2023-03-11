9a1c535897a87c8108295a39e1c9b754ddc77d5d
Peter Toth <tothpeter08@gmail.com>
2023-03-11 18:58:08 -0400

    Small cleanup

--- ruby/refactoring/gilded_rose/gilded_rose.rb
+++ ruby/refactoring/gilded_rose/gilded_rose.rb
@@ -1,21 +1,22 @@
-class GildedRose
+# frozen_string_literal: true
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,14 +30,14 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != 'Aged Brie'
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != 'Sulfuras, Hand of Ragnaros'
                 item.quality = item.quality - 1
               end
             end
@@ -62,7 +63,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
+  def to_s
+    '#{@name}, #{@sell_in}, #{@quality}'
   end
 end
--- ruby/refactoring/gilded_rose/spec/gilded_rose_spec.rb
+++ ruby/refactoring/gilded_rose/spec/gilded_rose_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require './gilded_rose'
 
 describe GildedRose do
