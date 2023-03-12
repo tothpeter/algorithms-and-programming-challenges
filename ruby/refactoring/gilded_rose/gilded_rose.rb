8a1479a378e979debb482167ef603e65c44a2294
Peter Toth <tothpeter08@gmail.com>
2023-03-11 20:07:08 -0400

    Remove old logic

--- ruby/refactoring/gilded_rose/gilded_rose.rb
+++ ruby/refactoring/gilded_rose/gilded_rose.rb
@@ -10,59 +10,11 @@ class GildedRose
       case item.name
       when 'Aged Brie'
         update_aged_brie(item)
-        next
       when 'Backstage passes to a TAFKAL80ETC concert'
         update_backstage_passes(item)
-        next
       when 'Sulfuras, Hand of Ragnaros'
-        next
       else
         update_base_case(item)
-        next
-      end
-
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = 0
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
       end
     end
   end
