172aaf03a9f37a26f6aab9555f445488d77b8e60
Peter Toth <tothpeter08@gmail.com>
2023-03-22 23:17:47 +0100

    Refactor the Fibonacci example

--- ruby/general/fibonacci/fibonacci.rb
+++ ruby/general/fibonacci/fibonacci.rb
@@ -1,21 +1,20 @@
-# 0, 1, 2, 3, 4, 5, 6, 7,  8
 # 0, 1, 1, 2, 3, 5, 8, 13, 21
 
-class Fib
-  def self.recursive n
-    return n if n < 2
+class Fibonacci
+  def self.recursive(nth_number)
+    return nth_number if nth_number < 2
 
-    recursive(n - 1) + recursive(n - 2)
+    recursive(nth_number - 1) + recursive(nth_number - 2)
   end
 
-  def self.iterative n
-    return n if n < 2
+  def self.iterative(nth_number)
+    return nth_number if nth_number < 2
 
     item_0 = 0
     item_1 = 1
     current = nil
 
-    (n - 1).times do
+    (nth_number - 1).times do
       current = item_0 + item_1
       item_0 = item_1
       item_1 = current
@@ -25,14 +24,18 @@ class Fib
   end
 end
 
-class Fiber
+class RecursiveFibonacciWithMemoize
+  def self.recursive(nth_number)
+    new.recursive(nth_number)
+  end
+
   def initialize
     @memo = { 0 => 0, 1 => 1 }
   end
 
-  def recursive_with_memoize n
-    return @memo[n] if @memo.has_key? n
+  def recursive(nth_number)
+    return @memo[nth_number] if @memo.has_key?(nth_number)
 
-    @memo[n] = recursive_with_memoize(n - 1) + recursive_with_memoize(n - 2)
+    @memo[nth_number] = recursive(nth_number - 1) + recursive(nth_number - 2)
   end
 end
--- ruby/general/fibonacci/spec/fibonacci_spec.rb
+++ ruby/general/fibonacci/spec/fibonacci_spec.rb
@@ -1,42 +1,40 @@
 require './fibonacci'
 
-describe Fib do
-  describe ".recursive" do
-    it "returns the nth fibonacci number" do
-      expect(Fib.recursive(0)).to eq(0)
-      expect(Fib.recursive(1)).to eq(1)
-      expect(Fib.recursive(2)).to eq(1)
-      expect(Fib.recursive(5)).to eq(5)
-      expect(Fib.recursive(7)).to eq(13)
-      expect(Fib.recursive(8)).to eq(21)
+describe Fibonacci do
+  describe 'recursive' do
+    it 'returns the nth fibonacci number' do
+      expect(described_class.recursive(0)).to eq(0)
+      expect(described_class.recursive(1)).to eq(1)
+      expect(described_class.recursive(2)).to eq(1)
+      expect(described_class.recursive(5)).to eq(5)
+      expect(described_class.recursive(7)).to eq(13)
+      expect(described_class.recursive(8)).to eq(21)
     end
   end
 
-  describe ".iterative" do
-    it "returns the nth fibonacci number" do
-      expect(Fib.iterative(0)).to eq(0)
-      expect(Fib.iterative(1)).to eq(1)
-      expect(Fib.iterative(2)).to eq(1)
-      expect(Fib.iterative(5)).to eq(5)
-      expect(Fib.iterative(7)).to eq(13)
-      expect(Fib.iterative(8)).to eq(21)
+  describe 'iterative' do
+    it 'returns the nth fibonacci number' do
+      expect(described_class.iterative(0)).to eq(0)
+      expect(described_class.iterative(1)).to eq(1)
+      expect(described_class.iterative(2)).to eq(1)
+      expect(described_class.iterative(5)).to eq(5)
+      expect(described_class.iterative(7)).to eq(13)
+      expect(described_class.iterative(8)).to eq(21)
     end
   end
 end
 
-describe Fiber do
-  describe "#recursive_with_memoize" do
-    it "returns the nth fibonacci number" do
-      expect(Fiber.new.recursive_with_memoize(0)).to eq(0)
-      expect(Fiber.new.recursive_with_memoize(1)).to eq(1)
-      expect(Fiber.new.recursive_with_memoize(2)).to eq(1)
-      expect(Fiber.new.recursive_with_memoize(5)).to eq(5)
-      expect(Fiber.new.recursive_with_memoize(7)).to eq(13)
-      expect(Fiber.new.recursive_with_memoize(8)).to eq(21)
-    end
+describe RecursiveFibonacciWithMemoize do
+  it 'returns the nth Fibonacci number' do
+    expect(described_class.recursive(0)).to eq(0)
+    expect(described_class.recursive(1)).to eq(1)
+    expect(described_class.recursive(2)).to eq(1)
+    expect(described_class.recursive(5)).to eq(5)
+    expect(described_class.recursive(7)).to eq(13)
+    expect(described_class.recursive(8)).to eq(21)
+  end
 
-    it "runs fast" do
-      expect(Fiber.new.recursive_with_memoize(60)).to eq(1548008755920)
-    end
+  it 'runs fast' do
+    expect(described_class.recursive(60)).to eq(1548008755920)
   end
 end
