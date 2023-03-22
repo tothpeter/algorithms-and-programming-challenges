0b998524f1e2a92beb710c140a8b24af82e64397
Peter Toth <tothpeter08@gmail.com>
2023-03-22 23:37:24 +0100

    Better template

--- ruby/#template/spec/01_spec.rb
+++ ruby/#template/spec/01_spec.rb
@@ -1,16 +1,7 @@
 # frozen_string_literal: true
 
 require './01'
-require '../spec_helper'
-
-examples = [
-  {
-    context: '',
-    input: [  ],
-    expected_output: nil
-  },
-]
 
 describe 'solution' do
-  test_batch_examples examples, :solution
+
 end
