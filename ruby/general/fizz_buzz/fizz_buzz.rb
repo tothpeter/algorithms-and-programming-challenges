59eecbd63219cd06ae343e9b2daca44dc93cdf88
Peter Toth <tothpeter08@gmail.com>
2023-03-22 23:03:29 +0100

    Refactor the fizz buzz challenge

--- ruby/general/fizz_buzz/fizz_buzz.rb
+++ ruby/general/fizz_buzz/fizz_buzz.rb
@@ -1,19 +1,15 @@
 class FizzBuzz
   def self.simple
-    result = []
-
     (1..100).each do |number|
       if number % 15 == 0
-        result << 'FizzBuzz'
+        print('FizzBuzz')
       elsif number % 3 == 0
-        result << 'Fizz'
+        print('Fizz')
       elsif number % 5 == 0
-        result << 'Buzz'
+        print('Buzz')
       else
-        result << number
+        print(number)
       end
     end
-
-    puts result.join ', '
   end
 end
--- ruby/general/fizz_buzz/spec/fizz_buzz_spec.rb
+++ ruby/general/fizz_buzz/spec/fizz_buzz_spec.rb
@@ -1,22 +1,11 @@
 require './fizz_buzz'
 
 describe FizzBuzz do
-  describe ".sample" do
-    it "returns the correct result in string" do
-      outstream, errorstream = capture_io do
-        FizzBuzz.simple
-      end
-
-      result = outstream.split ', '
-
-      expect(result[0]).to eq "1"
-      expect(result[1]).to eq "2"
-      expect(result[2]).to eq "Fizz"
-      expect(result[4]).to eq "Buzz"
-      expect(result[14]).to eq "FizzBuzz"
-      expect(result[29]).to eq "FizzBuzz"
-      expect(result[99]).to eq "Buzz\n"
-      expect(result.length).to eq 100
+  it 'prints the correct string to the console' do
+    outstream, errorstream = capture_io do
+      FizzBuzz.simple
     end
+
+    expect(outstream).to start_with('12Fizz4BuzzFizz78FizzBuzz11')
   end
 end
