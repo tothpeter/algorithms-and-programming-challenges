2035e9695c7228c17fba9df83f56ad0aeb46a59b
Peter Toth <tothpeter08@gmail.com>
2023-06-12 13:25:33 +0200

    Facelift the perm check Codility chellenge

--- ruby/codility.com/lessons/4_counting_elements/1_perm_check.rb
+++ ruby/codility.com/lessons/4_counting_elements/1_perm_check.rb
@@ -1,44 +1,47 @@
-# Check whether array List is a permutation.
+# Check whether array `numbers` is a permutation
+# https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/
 
 require 'set'
 
-def solution1(input_list)
-  # Does it start from one?
-  return 0 if input_list.min != 1
-  # Does it end with the right element?
-  return 0 if input_list.max != input_list.length
-  # Is it uniqe?
-  return 0 if input_list.uniq.length != input_list.length
+class Solution
+  def self.solution1(numbers)
+    # Does it start from one?
+    return 0 if numbers.min != 1
+    # Does it end with the right element?
+    return 0 if numbers.max != numbers.length
+    # Does it contain duplicates?
+    return 0 if numbers.uniq.length != numbers.length
 
-  1
-end
-
-
-def solution2 list
-  return 0 if list.empty?
-
-  seen = {}
-
-  list.each do |item|
-    return 0 if item > list.length
-    seen[item] = true
+    1
   end
 
-  list.length == seen.length ? 1 : 0
-end
 
-def solution3 list
-  return 0 if list.empty?
+  def self.solution2(numbers)
+    return 0 if numbers.min != 1
 
-  set = Set.new(1..(list.length))
+    seen = {}
 
-  list.each do |item|
-    set.delete item
+    numbers.each do |item|
+      return 0 if item > numbers.length
+      seen[item] = true
+    end
+
+    numbers.length == seen.length ? 1 : 0
   end
 
-  if set.empty?
-    return 1
-  else
-    return 0
+  def self.solution3(numbers)
+    return 0 if numbers.min != 1
+
+    expected_numbers = Set.new(1..(numbers.length))
+
+    numbers.each do |item|
+      expected_numbers.delete(item)
+    end
+
+    if expected_numbers.empty?
+      return 1
+    else
+      return 0
+    end
   end
 end
--- ruby/codility.com/lessons/4_counting_elements/spec/1_perm_check_spec.rb
+++ ruby/codility.com/lessons/4_counting_elements/spec/1_perm_check_spec.rb
@@ -1,49 +1,27 @@
 require './1_perm_check'
 
-context 'when there is a permutation' do
-  it 'returns 1' do
-    result = solution1([1,4,3,2])
+describe 'Solution #1' do
+  context 'when there is a permutation' do
+    it 'returns 1' do
+      result = Solution.solution1([1,4,3,2])
 
-    expect(result).to eq(1)
-  end
-end
-
-
-context 'when there is NO permutation' do
-  context 'sum is NOT okay' do
-    it 'returns 0' do
-      result = solution1([1,2,2])
-
-      expect(result).to eq(0)
+      expect(result).to eq(1)
     end
   end
 
-  context 'sum is okay but not unique' do
-    it 'returns 0' do
-      result = solution1([1,2,2,2,5])
 
-      expect(result).to eq(0)
-    end
-  end
-
-  it 'returns 0' do
-    result = solution1([2,3,4])
-
-    expect(result).to eq(0)
-  end
-
-  context 'sum is okay' do
-    context 'not uniq max is NOT okay' do
+  context 'when there is NO permutation' do
+    context 'sum is NOT okay' do
       it 'returns 0' do
-        result = solution1([1,2,2,5])
+        result = Solution.solution1([1,2,2])
 
         expect(result).to eq(0)
       end
     end
 
-    context 'not uniq max is okay' do
+    context 'sum is okay but contains duplicates' do
       it 'returns 0' do
-        result = solution1([1,2,2,5,5,6])
+        result = Solution.solution1([1,2,2,2,5])
 
         expect(result).to eq(0)
       end
@@ -51,9 +29,27 @@ context 'when there is NO permutation' do
 
     context 'does not start with 1' do
       it 'returns 0' do
-        result = solution1([2,2,2])
+        result = Solution.solution1([2,3,4])
 
         expect(result).to eq(0)
+      end
+    end
+
+    context 'sum is okay' do
+      context 'max is NOT okay and contains duplicates' do
+        it 'returns 0' do
+          result = Solution.solution1([1,2,2,5])
+
+          expect(result).to eq(0)
+        end
+      end
+
+      context 'max is okay but contains duplicates' do
+        it 'returns 0' do
+          result = Solution.solution1([1,2,2,5,5,6])
+
+          expect(result).to eq(0)
+        end
       end
     end
   end
